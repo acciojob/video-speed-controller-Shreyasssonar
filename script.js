c640d45c5be923eab5e7e5675387ce331825d26e
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2023-08-23 08:22:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,84 @@
-const inputs = document.querySelectorAll('.controls input');
+// Get the necessary elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const playbackSpeedInput = player.querySelector('.playbackSpeed');
+const skipBackButton = player.querySelector('.skipBack');
+const skipForwardButton = player.querySelector('.skipForward');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Play/pause functionality
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+
+// Update play/pause button
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+// Skip functionality
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+// Volume and playback speed sliders
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+// Playback speed
+playbackSpeedInput.addEventListener('change', function () {
+  video.playbackRate = this.value;
+});
+
+// Progress bar
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+video.addEventListener('timeupdate', handleProgress);
+
+// Scrubbing progress
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+progress.addEventListener('click', scrub);
+
+// Skip back and forward buttons
+skipBackButton.addEventListener('click', () => {
+  video.currentTime -= 10;
+});
+
+skipForwardButton.addEventListener('click', () => {
+  video.currentTime += 25;
+});
+
+// Initialize playback speed
+playbackSpeedInput.value = 1;
+video.playbackRate = 1;
+
+// Initialize video source
+video.src = 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4';
